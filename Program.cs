f213e96f2312bfa4850f395d882313ec933ca5c4
MatheusSgoda <matheussgoda05@gmail.com>
2025-04-09 10:55:26 -0300

    Refazer EX02 e fazer Ex03

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿public class Mesa01
+{
+    static void EX01()
+    {
+        int a = 10;
+        int b = 20;
+        int c = (a + b) / 2;
+        int aux = c;
+        c = c - 40;
+        int[] v = new int[4];
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(aux);
+        Console.WriteLine(c);
+
+        for (int i = 0; i < v.Length; i++)
+        {
+            if (i == 3)
+            {
+                v[i] = a + b + c;
+                Console.Write("V");
+            }
+            else
+            {
+                Console.Write("F");
+            }
+        }
+    }
+
+    static void EX02()
+    {
+        int a = 2;
+        int[] v = new int[5];
+
+        while (a < 6)
+        {
+            v[a] = 10 * a;
+            a += 1;
+        }
+        Console.WriteLine(a);
+    }
+
+
+
+    static void Main()
+    {
+        EX01();
+        EX02();
+
+    }
+}
